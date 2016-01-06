07b8cfc15783b802d6dc8e2931f4fbac9d20550c
Nick Strupat <nick.strupat@gmail.com>
2016-01-06 00:13:49 -0500

    Make the two extra ByRef value-type equality comparer methods static

--- Equality/StructEqualityComparer.cs
+++ Equality/StructEqualityComparer.cs
@@ -8,7 +8,7 @@ namespace Equality {
 		public Boolean Equals(T x, T y) => Struct.Equals(ref x, ref y);
 		public Int32 GetHashCode(T x) => Struct.GetHashCode(ref x);
 
-		public Boolean Equals(ref T x, ref T y) => Struct.Equals(ref x, ref y);
-		public Int32 GetHashCode(ref T x) => Struct.GetHashCode(ref x);
+		public static Boolean Equals(ref T x, ref T y) => Struct.Equals(ref x, ref y);
+		public static Int32 GetHashCode(ref T x) => Struct.GetHashCode(ref x);
 	}
 }
