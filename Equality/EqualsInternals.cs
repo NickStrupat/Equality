d2a8320da7d7053294ec99074bf35ca719a4b613
Nick Strupat <nick.strupat@intelex.com>
2016-01-06 19:12:29 -0500

    optimize for random-access collections and for dictionaries

--- Equality/EqualsInternals.cs
+++ Equality/EqualsInternals.cs
@@ -12,7 +12,7 @@ namespace Equality {
 		internal delegate Boolean ClassEquals<in T>(T x, T y) where T : class;
 
 		internal static class StaticStructCache<T> where T : struct {
-			public static readonly StructEquals<T> Func = GetStructEqualsFunc<T>(typeof (T));
+			public static readonly StructEquals<T> Func = GetStructEqualsFunc<T>(typeof(T));
 		}
 
 		internal static class StaticClassCache<T> where T : class {
@@ -27,23 +27,47 @@ namespace Equality {
 
 		private static readonly ConcurrentDictionary<Type, ClassEquals<Object>> DynamicCache = new ConcurrentDictionary<Type, ClassEquals<Object>>();
 
-		private static Boolean EnumerableEquals      <T>(IEnumerable<T> first, IEnumerable<T> second) => first.GetType().IsArray ? ArrayEquals((T[]) first, (T[]) second) : first.SequenceEqual(second);
-		private static Boolean EnumerableStructEquals<T>(IEnumerable<T> first, IEnumerable<T> second) where T : struct => first.SequenceEqual(second, StructEqualityComparer<T>.Default);
-		private static Boolean EnumerableClassEquals <T>(IEnumerable<T> first, IEnumerable<T> second) where T : class  => first.SequenceEqual(second, ClassEqualityComparer<T>.Default);
+		private static Boolean EnumerableEquals<T>(IEnumerable<T> first, IEnumerable<T> second) {
+			var dictionary = first as IDictionary;
+			if (dictionary != null)
+				return DictionaryEquals(dictionary, (IDictionary) second);
+			if (first is IList<T>)
+				return ArrayEquals(first, second);
+			if (first is IStructuralEquatable)
+				return StructuralComparisons.StructuralEqualityComparer.Equals(first, second);
+			return first.SequenceEqual(second);
+		}
 
-		private static Boolean ArrayEquals<T>(T[] first, T[] second) {
-			if (first.Length != second.Length)
+		private static Boolean EnumerableStructEquals<T>(IEnumerable<T> first, IEnumerable<T> second) where T : struct => first.SequenceEqual(second, StructEqualityComparer<T>.Default);
+		private static Boolean EnumerableClassEquals<T>(IEnumerable<T> first, IEnumerable<T> second) where T : class => first.SequenceEqual(second, ClassEqualityComparer<T>.Default);
+
+		private static Boolean ArrayEquals<T>(IEnumerable<T> first, IEnumerable<T> second) => ArrayEquals((IList<T>) first, (IList<T>) second);
+		private static Boolean ArrayEquals<T>(IList<T> first, IList<T> second) {
+			if (first.Count != second.Count)
 				return false;
 			var comparer = EqualityComparer<T>.Default;
-			for (var i = 0; i < first.Length; i++)
+			for (var i = 0; i < first.Count; i++)
 				if (!comparer.Equals(first[i], second[i]))
+					return false;
+			return true;
+		}
+
+		private static Boolean DictionaryEquals(IDictionary first, IDictionary second) {
+			if (first.Count != second.Count)
+				return false;
+			var firstKeys = first.Keys.Cast<Object>().OrderBy(x => x);
+			var secondKeys = second.Keys.Cast<Object>().OrderBy(x => x);
+			if (!firstKeys.SequenceEqual(secondKeys))
+				return false;
+			foreach (var key in firstKeys)
+				if (!first[key].Equals(second[key]))
 					return false;
 			return true;
 		}
 
 		private static void GenerateIL<T>(Type type, ILGenerator ilGenerator) {
 			Action<ILGenerator> loadFirstInstance = i => i.Emit(OpCodes.Ldarg_0);
-			if (typeof (T) != type) {
+			if (typeof(T) != type) {
 				var instanceLocal = ilGenerator.DeclareLocal(type);
 				loadFirstInstance = i => i.Emit(OpCodes.Ldloc, instanceLocal);
 				ilGenerator.Emit(OpCodes.Ldarg_0);
@@ -51,7 +75,7 @@ namespace Equality {
 				ilGenerator.Emit(OpCodes.Stloc, instanceLocal);
 			}
 			Action<ILGenerator> loadSecondInstance = i => i.Emit(OpCodes.Ldarg_1);
-			if (typeof (T) != type) {
+			if (typeof(T) != type) {
 				var instanceLocal = ilGenerator.DeclareLocal(type);
 				loadSecondInstance = i => i.Emit(OpCodes.Ldloc, instanceLocal);
 				ilGenerator.Emit(OpCodes.Ldarg_1);
@@ -83,14 +107,14 @@ namespace Equality {
 		}
 
 		private static void EmitMemberEqualityComparison(ILGenerator ilGenerator,
-		                                                 ConcurrentDictionary<Type, LocalBuilder> firstMemberLocalMap,
-		                                                 ConcurrentDictionary<Type, LocalBuilder> secondMemberLocalMap,
-		                                                 Action<ILGenerator> loadFirstInstance,
-		                                                 Action<ILGenerator> loadSecondInstance,
-		                                                 MemberInfo memberInfo,
-		                                                 Type memberType,
-		                                                 Label returnTrue,
-		                                                 Label nextMember) {
+														 ConcurrentDictionary<Type, LocalBuilder> firstMemberLocalMap,
+														 ConcurrentDictionary<Type, LocalBuilder> secondMemberLocalMap,
+														 Action<ILGenerator> loadFirstInstance,
+														 Action<ILGenerator> loadSecondInstance,
+														 MemberInfo memberInfo,
+														 Type memberType,
+														 Label returnTrue,
+														 Label nextMember) {
 			Action<ILGenerator> emitLoadFirstMember;
 			Action<ILGenerator> emitLoadSecondMember;
 			if (memberInfo is FieldInfo)
@@ -106,16 +130,16 @@ namespace Equality {
 			else {
 				MethodInfo opEquality;
 				Type t;
-				if (typeof (IEquatable<>).MakeGenericType(memberType).IsAssignableFrom(memberType)) {
+				if (typeof(IEquatable<>).MakeGenericType(memberType).IsAssignableFrom(memberType)) {
 					if (memberType.IsValueType) {
 						emitLoadFirstMember = SetEmitLoadFirstMemberForValueType(firstMemberLocalMap, memberInfo, memberType, emitLoadFirstMember);
 					}
 					else {
 						SetEmitLoadAndCompareForReferenceType(firstMemberLocalMap, secondMemberLocalMap, memberType, nextMember, ref emitLoadFirstMember, ref emitLoadSecondMember, ref emitComparison);
 					}
-					emitComparison = emitComparison.CombineDelegates(ilg => ilg.Emit(OpCodes.Call, memberType.GetMethod(nameof(Equals), new[] {memberType})));
+					emitComparison = emitComparison.CombineDelegates(ilg => ilg.Emit(OpCodes.Call, memberType.GetMethod(nameof(Equals), new[] { memberType })));
 				}
-				else if ((opEquality = memberType.GetMethod("op_Equality", new[] {memberType, memberType})) != null) {
+				else if ((opEquality = memberType.GetMethod("op_Equality", new[] { memberType, memberType })) != null) {
 					emitComparison = ilg => ilg.Emit(OpCodes.Call, opEquality);
 				}
 				else {
@@ -127,13 +151,13 @@ namespace Equality {
 						SetEmitLoadAndCompareForReferenceType(firstMemberLocalMap, secondMemberLocalMap, memberType, nextMember, ref emitLoadFirstMember, ref emitLoadSecondMember, ref emitComparison);
 					}
 					if (typeof(IEnumerable).IsAssignableFrom(memberType) && (t = memberType.GetInterfaces().SingleOrDefault(x => x.IsGenericType && x.GetGenericTypeDefinition() == typeof(IEnumerable<>))?.GetGenericArguments().Single()) != null)
-						emitComparison = emitComparison.CombineDelegates(ilg => ilg.Emit(OpCodes.Call, typeof (EqualsInternals).GetMethod(nameof(EnumerableEquals), BindingFlags.NonPublic | BindingFlags.Static).MakeGenericMethod(t)));
+						emitComparison = emitComparison.CombineDelegates(ilg => ilg.Emit(OpCodes.Call, typeof(EqualsInternals).GetMethod(nameof(EnumerableEquals), BindingFlags.NonPublic | BindingFlags.Static).MakeGenericMethod(t)));
 					else
-						emitComparison = emitComparison.CombineDelegates(ilg => ilg.Emit(OpCodes.Callvirt, memberType.GetMethod(nameof(Equals), new[] {typeof (Object)})));
+						emitComparison = emitComparison.CombineDelegates(ilg => ilg.Emit(OpCodes.Callvirt, memberType.GetMethod(nameof(Equals), new[] { typeof(Object) })));
 				}
 				emitComparison = emitComparison.CombineDelegates(ilg => ilg.Emit(OpCodes.Brtrue, nextMember));
 			}
-			
+
 			loadFirstInstance(ilGenerator);
 			emitLoadFirstMember(ilGenerator);
 			loadSecondInstance(ilGenerator);
@@ -145,13 +169,12 @@ namespace Equality {
 		}
 
 		private static void SetEmitLoadAndCompareForReferenceType(ConcurrentDictionary<Type, LocalBuilder> firstMemberLocalMap,
-		                                                                         ConcurrentDictionary<Type, LocalBuilder> secondMemberLocalMap,
-		                                                                         Type memberType,
-		                                                                         Label nextMember,
-		                                                                         ref Action<ILGenerator> emitLoadFirstMember,
-		                                                                         ref Action<ILGenerator> emitLoadSecondMember,
-		                                                                         ref Action<ILGenerator> emitComparison)
-		{
+																				 ConcurrentDictionary<Type, LocalBuilder> secondMemberLocalMap,
+																				 Type memberType,
+																				 Label nextMember,
+																				 ref Action<ILGenerator> emitLoadFirstMember,
+																				 ref Action<ILGenerator> emitLoadSecondMember,
+																				 ref Action<ILGenerator> emitComparison) {
 			LocalBuilder firstLocal = null;
 			emitLoadFirstMember = emitLoadFirstMember.CombineDelegates(ilg => {
 				firstLocal = firstMemberLocalMap.GetOrAdd(memberType, ilg.DeclareLocal);
